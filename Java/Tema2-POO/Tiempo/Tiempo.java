961ea5652ef98dcad9662cb91286c54f42c98f0c
cralr <rafamcruza10@gmail.com>
2019-02-18 18:26:35 +0100

    Cambio en clase tiempo

--- Java/Tema2-POO/Tiempo/Tiempo.java
+++ Java/Tema2-POO/Tiempo/Tiempo.java
@@ -6,15 +6,15 @@ package Tiempo;
 import java.util.Calendar;
 
 /**
- * Crea la clase Tiempo con los m√©todos suma y resta. Los objetos de la clase
+ * Crea la clase Tiempo con los mÈtodos suma y resta. Los objetos de la clase
  * Tiempo son intervalos de tiempo y se crean de la forma Tiempo t = new
- * Tiempo(1, 20, 30) donde los par√°metros que se le pasan al constructor son las
- * horas, los minutos y los segundos respectivamente. Crea el m√©todo toString
+ * Tiempo(1, 20, 30) donde los par·metros que se le pasan al constructor son las
+ * horas, los minutos y los segundos respectivamente. Crea el mÈtodo toString
  * para ver los intervalos de tiempo de la forma 10h 35m 5s. Si se suman por
- * ejemplo 30m 40s y 35m 20s el resultado deber√≠a ser 1h 6m 0s. Realiza un
+ * ejemplo 30m 40s y 35m 20s el resultado deberÌa ser 1h 6m 0s. Realiza un
  * programa de prueba para comprobar que la clase funciona bien.
  *
- * @author Rafael Miguel Cruz √Ålvarez
+ * @author Rafael Miguel Cruz ¡lvarez
  * @version 1.0
  */
 public class Tiempo {
@@ -30,9 +30,9 @@ public class Tiempo {
 
  
  /*
-  * M√©todo para sumar horas, minutos y segundos.
+  * MÈtodo para sumar horas, minutos y segundos.
   * 
-  * @param horas, minutos y segundos pasados por par√°metros.
+  * @param horas, minutos y segundos pasados por par·metros.
   */
 
  public void sumarTiempo(int horas, int minutos, int segundos) {
@@ -41,14 +41,14 @@ public class Tiempo {
      calendario.add(Calendar.MINUTE, minutos);
      calendario.add(Calendar.HOUR_OF_DAY, horas);
    } else {
-     System.out.println("√âste apartado es para sumar horas, minutos y segundos.");
+     System.out.println("…ste apartado es para sumar horas, minutos y segundos.");
    }
  }
 
  /*
-  * M√©todo para sumar horas, minutos y segundos.
+  * MÈtodo para sumar horas, minutos y segundos.
   * 
-  * @param horas, minutos y segundos pasados por par√°metros.
+  * @param horas, minutos y segundos pasados por par·metros.
   */
 
  public void restarTiempo(int horas, int minutos, int segundos) {
@@ -57,7 +57,7 @@ public class Tiempo {
      calendario.add(Calendar.MINUTE, minutos);
      calendario.add(Calendar.HOUR_OF_DAY, horas);
    } else {
-     System.out.println("√âste apartado es para restar horas, minutos y segundos.");
+     System.out.println("…ste apartado es para restar horas, minutos y segundos.");
    }
  }
 
