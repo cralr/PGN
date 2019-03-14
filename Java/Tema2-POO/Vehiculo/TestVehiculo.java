ecdf7ad06909322aa8b847593c13ce4d5dc70c30
cralr <rafamcruza10@gmail.com>
2019-03-14 18:03:42 +0100

    Prueba

--- Java/Tema2-POO/Vehiculo/TestVehiculo.java
+++ Java/Tema2-POO/Vehiculo/TestVehiculo.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 import Listado1.Fraccion;
 
 /**
- *VEHÕCULOS
+ *VEH√çCULOS
  *=========
  *1. Anda con la bicicleta
  *2. Haz el caballito con la bicicleta
@@ -18,49 +18,49 @@ import Listado1.Fraccion;
  *6. Ver kilometraje del coche
  *7. Ver kilometraje total
  *8. Salir
- *Elige una opciÛn (1-8):
+ *Elige una opci√≥n (1-8):
  *
- * @author Rafael Miguel Cruz ¡lvarez
+ * @author Rafael Miguel Cruz √Ålvarez
  * @version 1.0
  */
 public class TestVehiculo {
 
   public static void main(String[] args) {
     Coche Skoda = new Coche("Rojo");
-    Bicicleta BiciMontaÒa = new Bicicleta("Freno Disco");
+    Bicicleta BiciMonta√±a = new Bicicleta("Freno Disco");
     int opc;
     int km;
     Scanner entrada = new Scanner (System.in);
     
     do {
-      //Menu para elegir una opciÛn
+      //Menu para elegir una opci√≥n
       System.out.println("----Menu Vehiculos----");
-      System.out.println("1∫)Anda con la Bicicleta");
-      System.out.println("2∫)Haz el caballito con la Bicicleta.");
-      System.out.println("3∫)Anda con el Coche.");
-      System.out.println("4∫)Quema rueda con el Coche.");
-      System.out.println("5∫)Ver kilometraje de la bicicleta.");
-      System.out.println("6∫)Ver kilometraje del coche.");
-      System.out.println("7∫)Ver kilometraje total.");
-      System.out.println("8∫)Salir");
+      System.out.println("1)Anda con la Bicicleta");
+      System.out.println("2)Haz el caballito con la Bicicleta.");
+      System.out.println("3)Anda con el Coche.");
+      System.out.println("4)Quema rueda con el Coche.");
+      System.out.println("5)Ver kilometraje de la bicicleta.");
+      System.out.println("6)Ver kilometraje del coche.");
+      System.out.println("7)Ver kilometraje total.");
+      System.out.println("8)Salir");
     
-      System.out.print("Introduzca una opciÛn entre 1 y 8: ");
+      System.out.print("Introduzca una opci√≥n entre 1 y 8: ");
       opc = entrada.nextInt();
     
     
       switch(opc) {
         case 1:
-          System.out.println("Introduce el n˙mero de km recorridos con su bicicleta: ");
+          System.out.println("Introduce el n√∫mero de km recorridos con su bicicleta: ");
           km = entrada.nextInt();
-          BiciMontaÒa.recorrer(km);
+          BiciMonta√±a.recorrer(km);
           break;
           
         case 2:
-          BiciMontaÒa.hacerCaballito();
+          BiciMonta√±a.hacerCaballito();
           break;
           
         case 3:
-          System.out.println("Introduce el n˙mero de km recorridos con su coche: ");
+          System.out.println("Introduce el nÔøΩmero de km recorridos con su coche: ");
           km = entrada.nextInt();
           Skoda.recorrer(km);
           break;
@@ -70,7 +70,7 @@ public class TestVehiculo {
           break;
           
         case 5: 
-          System.out.println("Ha recorrido con su bicicleta "+BiciMontaÒa.getKilometrosRecorridos()+" kms.");
+          System.out.println("Ha recorrido con su bicicleta "+BiciMonta√±a.getKilometrosRecorridos()+" kms.");
           break;
           
         case 6: 
--- Java/Tema2-POO/Vehiculo/Vehiculo.java
+++ Java/Tema2-POO/Vehiculo/Vehiculo.java
@@ -4,14 +4,14 @@
 package Vehiculo;
 
 /**
- * Crea la clase Vehiculo, asÌ como las clases Bicicleta y Coche como subclases
+ * Crea la clase Vehiculo, asÔøΩ como las clases Bicicleta y Coche como subclases
  * de la primera. Para la clase Vehiculo, crea los atributos de clase
- * vehiculosCreados y kilometrosTotales, asÌ como el atributo de instancia
- * kilometrosRecorridos. Crea tambiÈn alg˙n mÈtodo especÌfico para cada una de
- * las subclases. Prueba las clases creadas mediante un programa con un men˙
- * como el que se muestra a continuaciÛn:
+ * vehiculosCreados y kilometrosTotales, asÔøΩ como el atributo de instancia
+ * kilometrosRecorridos. Crea tambi√©n alg√∫n m√©todo espec√≠fico para cada una de
+ * las subclases. Prueba las clases creadas mediante un programa con un men√∫
+ * como el que se muestra a continuaci√≥n:
  *
- *VEHÕCULOS
+ *VEHÔøΩCULOS
  *=========
  *1. Anda con la bicicleta
  *2. Haz el caballito con la bicicleta
@@ -21,9 +21,9 @@ package Vehiculo;
  *6. Ver kilometraje del coche
  *7. Ver kilometraje total
  *8. Salir
- *Elige una opciÛn (1-8):
+ *Elige una opci√≥n (1-8):
  *
- * @author Rafael Miguel Cruz ¡lvarez
+ * @author Rafael Miguel Cruz √Ålvarez
  * @version 1.0
  */
 public abstract class Vehiculo {
@@ -32,11 +32,11 @@ public abstract class Vehiculo {
   private static int vehiculosCreados=0;
   private static int kilometrosTotales=0;
   
-  //MÈtodos de la clase 
+  //MÔøΩtodos de la clase 
   
   /**
-   * MÈtodo de la clase que devolver· el total de kilÛmetros recorridos
-   * @return kilometrosTotales kilÛmetros total recorridos
+   * M√©todo de la clase que devuelve el total de kil√≥metros recorridos
+   * @return kilometrosTotales kil√≥metros total recorridos
    */
   public static int getKilometrosTotal() {
     return Vehiculo.kilometrosTotales;
@@ -61,8 +61,8 @@ public abstract class Vehiculo {
   }
 
    /**
-    * MÈtodo para obtener el total de km de cada vehÌculo por separado (coche u bicicleta) y el total de km de los vehÌculos.
-    * @param kilometros N˙mero de km introducidos por teclado.
+    * MÔøΩtodo para obtener el total de km de cada veh√≠culo por separado (coche u bicicleta) y el total de km de los veh√≠culos.
+    * @param kilometros N√∫mero de km introducidos por teclado.
     */
   public void recorrer(int kilometros) {
     this.kilometrosRecorridos+=kilometros;
