c1a800318446d61cf8a86e614292ab2d72614905
cralr <rafamcruza10@gmail.com>
2019-04-01 17:49:05 +0200

    Añadido de ejercicios

--- Java/Tema2-POO/EjercicioExamen/TestMes.java
+++ Java/Tema2-POO/EjercicioExamen/TestMes.java
@@ -14,83 +14,167 @@ import java.util.Scanner;
  */
 
 public class TestMes {
-   public static void main(String[] args) throws ParseException {
+   public static void main(String[] args) {
     Scanner entrada = new Scanner (System.in);
     int opcion;
-    Date fecha = null;
-  
-    do {
-      //Menu para elegir una opción
-      System.out.println("----Menu Mes----");
-      System.out.println("1º)Introducir Fecha.");
-      System.out.println("2º)Sumar días.");
-      System.out.println("3º)Restar días.");
-      System.out.println("4º)Mostrar fechas.");
-      System.out.println("5º)Comparar fechas.");
-      System.out.println("6º)Terminar.");
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); 
+    Calendar calendario1 = Calendar.getInstance();
+    Calendar calendario2 = Calendar.getInstance();
+    int diaSumRes;
+    String fecha="";
     
+    do {
+      mostrarMenu();
       System.out.println("Introduzca una opción entre 1 y 5: ");
       opcion = entrada.nextInt();
     
     
       switch(opcion) {
         case 1:
-            System.out.println("Ingrese la fecha de modo(dd/mm/aaaa)");
-            String fechaString = entrada.next();
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-            fecha = sdf.parse(fechaString);
-            Calendar calendario = Calendar.getInstance();
-            calendario.setTime(fecha);
+            calendario1 = Verifica();
+            fecha=sdf.format(calendario1.getTime());
           break;
-        /*case 2:
-            System.out.println("Introduce el número de días a sumar a la fecha");
-            int dia=entrada.nextInt();
-            f1.sumarDias(dia);
-            System.out.println();
+        case 2:
+            if (fecha == "") {
+              System.out.println("No hay fecha, introduzca una.");
+            }
+            else {
+              System.out.println("Introduce los días a sumar.");
+              diaSumRes = entrada.nextInt();
+              calendario1.add(Calendar.DATE, diaSumRes);
+              System.out.println("La fecha es: "+ sdf.format(calendario1.getTime()));
+            }
           break;
         case 3:
-            System.out.println("Introduce el número de días a restar a la fecha");
-            dia=entrada.nextInt();
-            f1.restarDias(dia);
-            System.out.println();
-          break;*/
+          if (fecha == "") {
+            System.out.println("No hay fecha, introduzca una.");
+          }
+          else {
+            System.out.println("Introduce los días a restar.");
+            diaSumRes = entrada.nextInt();
+            calendario1.add(Calendar.DATE, -diaSumRes);
+            System.out.println("La fecha es: "+ sdf.format(calendario1.getTime()));
+          }
+          break;
         case 4:
-          System.out.println(fecha);
+          if (fecha == "") {
+            System.out.println("No hay fecha, introduzca una.");
+          }else {
+            System.out.println("Comparamos dos fechas: La guardada y la introducida.");
+            calendario2 = Verifica();
+            
+            if (calendario1.compareTo(calendario2)>0){
+                System.out.println("La fecha guardada es mayor que la introducida.");
+            }else {
+                System.out.println("La fecha guardada es menor que la introducida.");
+            }
+          }
           break;
         case 5:
-          
+            if (fecha == "") {
+              System.out.println("No hay Fecha, introduzca una.");
+            }else {
+              System.out.println("La fecha guardada es: " + sdf.format(calendario1.getTime()));
+            }
         break;
         default:
           System.out.println("Fin del Programa");
       }
     }while(opcion!=5);
    }
-}
 
-/*
- * Método para sumar días a la fecha
- * 
- * @param dia Es el número de días a sumar.
- */
-/*
-public void sumarDias(int dia) {
-  if (dia > 0) {
-    fecha.add(Calendar.DAY_OF_MONTH, dia);
-  } else {
-    System.out.println("Éste apartado es para sumar días.");
-  }
-}
 
-/*
- * Método para restar días a la fecha
- * 
- * @param dia Es el número de días a restar.
- */
-/*
-public void restarDias(int dia) {
-  if (dia < 0) {
-    fecha.add(Calendar.DAY_OF_MONTH, dia);
-  } else {
-    System.out.println("Éste apartado es para restar días.");
+  public static void mostrarMenu() {
+    //Menu para elegir una opción
+    System.out.println("----Menu Mes----");
+    System.out.println("1º)Introducir Fecha.");
+    System.out.println("2º)Sumar días.");
+    System.out.println("3º)Restar días.");
+    System.out.println("4º)Comparar Fechas.");
+    System.out.println("5º)Mostrar fechas.");
+    System.out.println("6º)Terminar.");
   }
-}*/
+  
+  public static Calendar Verifica() {
+    Scanner s = new Scanner(System.in);
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    Calendar calendario = Calendar.getInstance();
+    String fecha;
+    String verifica;
+    
+    do {
+        System.out.println("Introduzca la fecha: ");        
+        fecha = s.nextLine();
+        calendario= obtenerFecha(fecha);
+        verifica = sdf.format(calendario.getTime());
+        
+        if (!fecha.equals(verifica)) {
+          System.out.println("Fecha mal introducida");
+        }
+        }while(!fecha.equals(verifica));
+    return calendario;
+  }
+  
+  public static Calendar obtenerFecha(String Fecha) {
+    Calendar calendario = Calendar.getInstance();
+    String[] parts;
+    int d,m,y;
+    
+    if (Fecha.contains("/") && Fecha.length()==10) {
+      parts = Fecha.split("/");
+      d = Integer.parseInt(parts[0]);
+      m = Integer.parseInt(parts[1]);
+      y = Integer.parseInt(parts[2]);
+      
+      switch (m) {
+      case 1:
+        calendario.set(y, Calendar.JANUARY, d);
+        break;
+      case 2:
+        calendario.set(y, Calendar.FEBRUARY, d);
+        break;
+      case 3:
+        calendario.set(y, Calendar.MARCH, d);
+        break;
+      case 4:
+        calendario.set(y, Calendar.APRIL, d);
+        break;
+      case 5:
+        calendario.set(y, Calendar.MAY, d);
+        break;
+      case 6:
+        calendario.set(y, Calendar.JUNE, d);
+        break;
+      case 7:
+        calendario.set(y, Calendar.JULY, d);
+        break;
+      case 8:
+        calendario.set(y, Calendar.AUGUST, d);
+        break;
+      case 9:
+        calendario.set(y, Calendar.SEPTEMBER, d);
+        break;
+      case 10:
+        calendario.set(y, Calendar.OCTOBER, d);
+        break;
+      case 11:
+        calendario.set(y, Calendar.NOVEMBER, d);
+        break;
+      case 12:
+        calendario.set(y, Calendar.DECEMBER, d);
+        break;
+      default:
+        calendario.set(0, 0, 0);
+        break;
+      }
+    }else {
+      calendario.set(0, 0, 0);
+    }
+    return calendario;
+  }
+  
+  
+  
+  
+  
+}
--- Java/Tema2-POO/Tiempo/Tiempo.java
+++ Java/Tema2-POO/Tiempo/Tiempo.java
@@ -1,70 +1,155 @@
-/**
- * 
- */
 package Tiempo;
 
-import java.util.Calendar;
-
 /**
- * Crea la clase Tiempo con los métodos suma y resta. Los objetos de la clase
+ * Crea la clase Tiempo con los mÃ©todos suma y resta. Los objetos de la clase
  * Tiempo son intervalos de tiempo y se crean de la forma Tiempo t = new
- * Tiempo(1, 20, 30) donde los parámetros que se le pasan al constructor son las
- * horas, los minutos y los segundos respectivamente. Crea el método toString
+ * Tiempo(1, 20, 30) donde los parÃ¡metros que se le pasan al constructor son las
+ * horas, los minutos y los segundos respectivamente. Crea el mï¿½todo toString
  * para ver los intervalos de tiempo de la forma 10h 35m 5s. Si se suman por
- * ejemplo 30m 40s y 35m 20s el resultado debería ser 1h 6m 0s. Realiza un
+ * ejemplo 30m 40s y 35m 20s el resultado deberÃ­a ser 1h 6m 0s. Realiza un
  * programa de prueba para comprobar que la clase funciona bien.
  *
- * @author Rafael Miguel Cruz Álvarez
+ * @author Rafael Miguel Cruz Ãlvarez
  * @version 1.0
  */
+
 public class Tiempo {
-//atributos
- Calendar calendario;
 
- /**
-  * Constructor
-  */
- public Tiempo() {
-   calendario = Calendar.getInstance(); // getInstance() se usa para poder obtener una instancia que devuelve la hora actual.
- }
+  private int horas;
+  private int minutos;
+  private int segundos;
 
- 
- /*
-  * Método para sumar horas, minutos y segundos.
-  * 
-  * @param horas, minutos y segundos pasados por parámetros.
-  */
+  /**
+   * Constructor
+   * 
+   * @param horas
+   * @param minutos
+   * @param segundos
+   */
+  public Tiempo(int horas, int minutos, int segundos) {
+    if (segundos >= 59) {
+      minutos += segundos / 60;
+      segundos = segundos % 60;
+    }
+    if (minutos >= 60) {
+      horas += minutos / 60;
+      minutos = minutos % 60;
+    }
 
- public void sumarTiempo(int horas, int minutos, int segundos) {
-   if (horas > 0 && minutos >0 && segundos>0) {
-     calendario.add(Calendar.SECOND, segundos);
-     calendario.add(Calendar.MINUTE, minutos);
-     calendario.add(Calendar.HOUR_OF_DAY, horas);
-   } else {
-     System.out.println("Éste apartado es para sumar horas, minutos y segundos.");
-   }
- }
+    setHoras(horas);
+    setMinutos(minutos);
+    setSegundos(segundos);
+  }
 
- /*
-  * Método para sumar horas, minutos y segundos.
-  * 
-  * @param horas, minutos y segundos pasados por parámetros.
-  */
+  /**
+   * @return the horas
+   */
+  public int getHoras() {
+    return horas;
+  }
 
- public void restarTiempo(int horas, int minutos, int segundos) {
-   if (horas < 0 && minutos < 0 && segundos < 0) {
-     calendario.add(Calendar.SECOND, segundos);
-     calendario.add(Calendar.MINUTE, minutos);
-     calendario.add(Calendar.HOUR_OF_DAY, horas);
-   } else {
-     System.out.println("Éste apartado es para restar horas, minutos y segundos.");
-   }
- }
+  /**
+   * @param horas the horas to set
+   */
+  public void setHoras(int horas) {
+    this.horas = horas;
+  }
 
- @Override
- public String toString() {
-   return calendario.get(Calendar.HOUR_OF_DAY) + "h " + calendario.get(Calendar.MINUTE) + "m "
-       + calendario.get(Calendar.SECOND) + "s";
- }
+  /**
+   * @return the minutos
+   */
+  public int getMinutos() {
+    return minutos;
+  }
+
+  /**
+   * @param minutos the minutos to set
+   */
+  public void setMinutos(int minutos) {
+    this.minutos = minutos;
+    while (this.minutos >= 60) {
+      this.minutos -= 60;
+      this.horas++;
+    }
+  }
+
+  /**
+   * @return the segundos
+   */
+  public int getSegundos() {
+    return segundos;
+  }
+
+  /**
+   * @param segundos the segundos to set
+   */
+  public void setSegundos(int segundos) {
+    this.segundos = segundos;
+  }
+
+  /**
+   * Suma 2 rangos de tiempo
+   * 
+   * @param horas
+   * @param minutos
+   * @param segundos
+   */
+  public void sumar(int horas, int minutos, int segundos) {
+    this.segundos += segundos;
+    while (this.segundos >= 60) {
+      this.segundos -= 60;
+      this.minutos++;
+    }
+    this.minutos += minutos;
+    while (this.minutos >= 60) {
+      this.minutos -= 60;
+      this.horas++;
+    }
+    this.horas += horas;
+  }
+
+  /**
+   * Suma 2 rangos de tiempo
+   * 
+   * @param t
+   */
+  public void sumar(Tiempo t) {
+    this.sumar(t.getHoras(), t.getMinutos(), t.getSegundos());
+  }
+
+  /**
+   * Resta 2 rangos de tiempo
+   * 
+   * @param horas
+   * @param minutos
+   * @param segundos
+   */
+  public void restar(int horas, int minutos, int segundos) {
+    this.segundos -= segundos;
+    if (this.segundos < 0) {
+      this.segundos += 60;
+      this.minutos--;
+    }
+    this.minutos -= minutos;
+    if (this.minutos < 0) {
+      this.minutos += 60;
+      this.horas--;
+    }
+    this.horas -= horas;
+  }
+
+  /**
+   * Resta 2 rangos de tiempo
+   * 
+   * @param t
+   */
+  public void restar(Tiempo t) {
+    this.restar(t.getHoras(), t.getMinutos(), t.getSegundos());
+  }
+
+  @Override
+  public String toString() {
+    return horas + "h " + minutos + "m " + segundos + "s";
+  }
 
 }
